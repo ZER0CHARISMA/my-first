a0b598c530dc961d4586af847933db40655fdb83
mohammad <iamm5458@gmail.com>
2017-10-19 22:59:08 +0330

    1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,12 +1,47 @@
-﻿using System;
-
-namespace ConsoleApp1
+﻿namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            System.Console.Write("how many numbers do you have? ");
+
+            string strNumberCount =
+                System.Console.ReadLine();
+
+            int intNumberCount =
+                System.Convert.ToInt32(strNumberCount);
+
+            int[] intNumbers =
+                new int[intNumberCount];
+
+            for (int intIndexNumber = 1; intIndexNumber <= intNumberCount; intIndexNumber++)
+            {
+                string strMassege =
+                    string.Format("[{0}] -> ", intIndexNumber);
+
+                System.Console.Write(strMassege);
+
+                string strNumber =
+                    System.Console.ReadLine();
+
+                int intNumber =
+                    System.Convert.ToInt32(strNumber);
+
+                intNumbers[intIndexNumber - 1] =
+                    intNumber;
+            }
+            int intSum = 0;
+
+            for (int intIndex = 0; intIndex <= intNumberCount - 1; intIndex++)
+            {
+                intSum +=
+                    intNumbers[intIndex];
+            }
+
+            System.Console.Write("the sum of your number is: " + intSum);
+
+            System.Console.ReadLine();
         }
     }
 }
